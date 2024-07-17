b681e62907d551162a28ea0dd2c08065a3bbb0b6
Sybille Peters <sypets@gmx.de>
2024-07-17 09:46:38 +0200

    [BUGFIX] Fix typo

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Sypets\SypetsXclassDi\Backend\PreviewRenderer\Hook\StandardContentPreviewRenderer as XclassStandardContentPreviewRenderer;
 use TYPO3\CMS\Backend\Preview\StandardContentPreviewRenderer as CoreStandardContentPreviewRenderer;
 
-defined(TYPO3) or die();
+defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][CoreStandardContentPreviewRenderer::class] = [
     'className' => XclassStandardContentPreviewRenderer::class
